8c40c7ded52075736063436b0ff68502ef51c209
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2020-12-29 02:47:55 +0100

    formatting

--- mediaplayer/src/decoder_video_libdce.cpp
+++ mediaplayer/src/decoder_video_libdce.cpp
@@ -117,13 +117,13 @@ STATUS DecoderVideoLibDCE::init(Demuxer *demuxer, Display *display) {
 	_display = display;
 	if (display->getHandle(&displayHandle) != S_OK) {
 		log->printf("DecoderVideoLibDCE::init(): failed get display handle!\n");
-        goto fail;
+		goto fail;
 	}
 
 	StreamVideoInfo info;
 	if (demuxer->getVideoStreamInfo(&info) != S_OK) {
 		log->printf("DecoderVideoLibDCE::init(): demuxer->getVideoStreamInfo() failed\n");
-        goto fail;
+		goto fail;
 	}
 
 	_numFrameBuffers = 3;
@@ -174,14 +174,14 @@ STATUS DecoderVideoLibDCE::init(Demuxer *demuxer, Display *display) {
 			break;
 		default:
 			log->printf("DecoderVideoLibDCE::init(): not supported profile level: %d\n", info.profileLevel);
-	        goto fail;
+			goto fail;
 		}
 		dpbSizeInFrames = MIN(16, maxDpb / ((info.width / 16) * (info.height / 16)));
 		_numFrameBuffers = IVIDEO2_MAX_IO_BUFFERS;
 		break;
 	default:
 		log->printf("DecoderVideoLibDCE::init(): not supported codec!\n");
-        goto fail;
+		goto fail;
 	}
 
 	_numFrameBuffers += 2; // for display buffering
@@ -193,200 +193,200 @@ STATUS DecoderVideoLibDCE::init(Demuxer *demuxer, Display *display) {
 	_omapDev = dce_init();
 	if (_omapDev == nullptr) {
 		log->printf("DecoderVideoLibDCE::init(): failed init dce!\n");
-        goto fail;
+		goto fail;
 	}
 
 	_codecEngine = Engine_open((String)"ivahd_vidsvr", nullptr, &engineError);
 	if (!_codecEngine) {
 		log->printf("DecoderVideoLibDCE::init(): failed open codec engine!\n");
-        goto fail;
+		goto fail;
 	}
 
 	switch (_codecId) {
-    case CODEC_ID_H264:
-        _codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IH264VDEC_Params));
-        break;
-    case CODEC_ID_MPEG4:
-    	_codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IMPEG4VDEC_Params));
-        break;
-    case CODEC_ID_MPEG1VIDEO:
-    case CODEC_ID_MPEG2VIDEO:
-    	_codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IMPEG2VDEC_Params));
-        break;
-    case CODEC_ID_WMV3:
-    case CODEC_ID_VC1:
-    	_codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IVC1VDEC_Params));
-        break;
-    default:
+	case CODEC_ID_H264:
+		_codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IH264VDEC_Params));
+		break;
+	case CODEC_ID_MPEG4:
+		_codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IMPEG4VDEC_Params));
+		break;
+	case CODEC_ID_MPEG1VIDEO:
+	case CODEC_ID_MPEG2VIDEO:
+		_codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IMPEG2VDEC_Params));
+		break;
+	case CODEC_ID_WMV3:
+	case CODEC_ID_VC1:
+		_codecParams = (VIDDEC3_Params *)dce_alloc(sizeof(IVC1VDEC_Params));
+		break;
+	default:
 		log->printf("DecoderVideoLibDCE::init(): Unsupported codec %08x\n", _codecId);
-        goto fail;
-    }
+		goto fail;
+	}
 
-    if (!_codecParams) {
+	if (!_codecParams) {
 		log->printf("DecoderVideoLibDCE::init(): Error allocation with dce_alloc()\n");
-        goto fail;
-    }
+		goto fail;
+	}
 
-    _codecParams->maxWidth = _frameWidth;
-    _codecParams->maxHeight = _frameHeight;
-    _codecParams->maxFrameRate = 30000;
-    _codecParams->maxBitRate = 10000000;
-    _codecParams->dataEndianness = XDM_BYTE;
-    _codecParams->forceChromaFormat = XDM_YUV_420SP;
-    _codecParams->operatingMode = IVIDEO_DECODE_ONLY;
-    _codecParams->displayDelay = IVIDDEC3_DISPLAY_DELAY_AUTO;
-    _codecParams->displayBufsMode = IVIDDEC3_DISPLAYBUFS_EMBEDDED;
-    _codecParams->inputDataMode = IVIDEO_ENTIREFRAME;
-    _codecParams->outputDataMode = IVIDEO_ENTIREFRAME;
-    _codecParams->numInputDataUnits = 0;
-    _codecParams->numOutputDataUnits = 0;
-    _codecParams->errorInfoMode = IVIDEO_ERRORINFO_OFF;
-    _codecParams->metadataType[0] = IVIDEO_METADATAPLANE_NONE;
-    _codecParams->metadataType[1] = IVIDEO_METADATAPLANE_NONE;
-    _codecParams->metadataType[2] = IVIDEO_METADATAPLANE_NONE;
+	_codecParams->maxWidth = _frameWidth;
+	_codecParams->maxHeight = _frameHeight;
+	_codecParams->maxFrameRate = 30000;
+	_codecParams->maxBitRate = 10000000;
+	_codecParams->dataEndianness = XDM_BYTE;
+	_codecParams->forceChromaFormat = XDM_YUV_420SP;
+	_codecParams->operatingMode = IVIDEO_DECODE_ONLY;
+	_codecParams->displayDelay = IVIDDEC3_DISPLAY_DELAY_AUTO;
+	_codecParams->displayBufsMode = IVIDDEC3_DISPLAYBUFS_EMBEDDED;
+	_codecParams->inputDataMode = IVIDEO_ENTIREFRAME;
+	_codecParams->outputDataMode = IVIDEO_ENTIREFRAME;
+	_codecParams->numInputDataUnits = 0;
+	_codecParams->numOutputDataUnits = 0;
+	_codecParams->errorInfoMode = IVIDEO_ERRORINFO_OFF;
+	_codecParams->metadataType[0] = IVIDEO_METADATAPLANE_NONE;
+	_codecParams->metadataType[1] = IVIDEO_METADATAPLANE_NONE;
+	_codecParams->metadataType[2] = IVIDEO_METADATAPLANE_NONE;
 
-    switch (_codecId) {
-    case CODEC_ID_H264:
-    	_frameWidth = ALIGN2(_frameWidth + (32 * 2), 7);
-    	_frameHeight = _frameHeight + 4 * 24;
-    	_codecParams->size = sizeof(IH264VDEC_Params);
-        ((IH264VDEC_Params *)_codecParams)->dpbSizeInFrames = dpbSizeInFrames;//IH264VDEC_DPB_NUMFRAMES_AUTO;
-        ((IH264VDEC_Params *)_codecParams)->pConstantMemory = 0;
-        ((IH264VDEC_Params *)_codecParams)->bitStreamFormat = IH264VDEC_BYTE_STREAM_FORMAT;
-        ((IH264VDEC_Params *)_codecParams)->errConcealmentMode = IH264VDEC_APPLY_CONCEALMENT;
-        ((IH264VDEC_Params *)_codecParams)->temporalDirModePred = IH264VDEC_ENABLE_TEMPORALDIRECT;
-        ((IH264VDEC_Params *)_codecParams)->svcExtensionFlag = IH264VDEC_DISABLE_SVCEXTENSION;
-        ((IH264VDEC_Params *)_codecParams)->svcTargetLayerDID = IH264VDEC_TARGET_DID_DEFAULT;
-        ((IH264VDEC_Params *)_codecParams)->svcTargetLayerTID = IH264VDEC_TARGET_TID_DEFAULT;
-        ((IH264VDEC_Params *)_codecParams)->svcTargetLayerQID = IH264VDEC_TARGET_QID_DEFAULT;
-        ((IH264VDEC_Params *)_codecParams)->presetLevelIdc = IH264VDEC_MAXLEVELID;
-        ((IH264VDEC_Params *)_codecParams)->presetProfileIdc = IH264VDEC_PROFILE_ANY;
-        ((IH264VDEC_Params *)_codecParams)->detectCabacAlignErr = IH264VDEC_DISABLE_CABACALIGNERR_DETECTION;
-        ((IH264VDEC_Params *)_codecParams)->detectIPCMAlignErr = IH264VDEC_DISABLE_IPCMALIGNERR_DETECTION;
-        ((IH264VDEC_Params *)_codecParams)->debugTraceLevel = IH264VDEC_DEBUGTRACE_LEVEL0; // 0 - 3
-        ((IH264VDEC_Params *)_codecParams)->lastNFramesToLog = 0;
-        ((IH264VDEC_Params *)_codecParams)->enableDualOutput = IH264VDEC_DUALOUTPUT_DISABLE;
-        ((IH264VDEC_Params *)_codecParams)->processCallLevel = FALSE; // TRUE - for interlace
-        ((IH264VDEC_Params *)_codecParams)->enableWatermark = IH264VDEC_WATERMARK_DISABLE;
-        ((IH264VDEC_Params *)_codecParams)->decodeFrameType = IH264VDEC_DECODE_ALL;
-        _codecHandle = VIDDEC3_create(_codecEngine, (String)"ivahd_h264dec", _codecParams);
+	switch (_codecId) {
+	case CODEC_ID_H264:
+		_frameWidth = ALIGN2(_frameWidth + (32 * 2), 7);
+		_frameHeight = _frameHeight + 4 * 24;
+		_codecParams->size = sizeof(IH264VDEC_Params);
+		((IH264VDEC_Params *)_codecParams)->dpbSizeInFrames = dpbSizeInFrames;//IH264VDEC_DPB_NUMFRAMES_AUTO;
+		((IH264VDEC_Params *)_codecParams)->pConstantMemory = 0;
+		((IH264VDEC_Params *)_codecParams)->bitStreamFormat = IH264VDEC_BYTE_STREAM_FORMAT;
+		((IH264VDEC_Params *)_codecParams)->errConcealmentMode = IH264VDEC_APPLY_CONCEALMENT;
+		((IH264VDEC_Params *)_codecParams)->temporalDirModePred = IH264VDEC_ENABLE_TEMPORALDIRECT;
+		((IH264VDEC_Params *)_codecParams)->svcExtensionFlag = IH264VDEC_DISABLE_SVCEXTENSION;
+		((IH264VDEC_Params *)_codecParams)->svcTargetLayerDID = IH264VDEC_TARGET_DID_DEFAULT;
+		((IH264VDEC_Params *)_codecParams)->svcTargetLayerTID = IH264VDEC_TARGET_TID_DEFAULT;
+		((IH264VDEC_Params *)_codecParams)->svcTargetLayerQID = IH264VDEC_TARGET_QID_DEFAULT;
+		((IH264VDEC_Params *)_codecParams)->presetLevelIdc = IH264VDEC_MAXLEVELID;
+		((IH264VDEC_Params *)_codecParams)->presetProfileIdc = IH264VDEC_PROFILE_ANY;
+		((IH264VDEC_Params *)_codecParams)->detectCabacAlignErr = IH264VDEC_DISABLE_CABACALIGNERR_DETECTION;
+		((IH264VDEC_Params *)_codecParams)->detectIPCMAlignErr = IH264VDEC_DISABLE_IPCMALIGNERR_DETECTION;
+		((IH264VDEC_Params *)_codecParams)->debugTraceLevel = IH264VDEC_DEBUGTRACE_LEVEL0; // 0 - 3
+		((IH264VDEC_Params *)_codecParams)->lastNFramesToLog = 0;
+		((IH264VDEC_Params *)_codecParams)->enableDualOutput = IH264VDEC_DUALOUTPUT_DISABLE;
+		((IH264VDEC_Params *)_codecParams)->processCallLevel = FALSE; // TRUE - for interlace
+		((IH264VDEC_Params *)_codecParams)->enableWatermark = IH264VDEC_WATERMARK_DISABLE;
+		((IH264VDEC_Params *)_codecParams)->decodeFrameType = IH264VDEC_DECODE_ALL;
+		_codecHandle = VIDDEC3_create(_codecEngine, (String)"ivahd_h264dec", _codecParams);
 		log->printf("DecoderVideoLibDCE::init(): using ivahd_h264dec\n");
-        break;
-    case CODEC_ID_MPEG4:
-    	_frameWidth = ALIGN2(_frameWidth + 32, 7);
-    	_frameHeight = _frameHeight + 32;
-    	_codecParams->size = sizeof(IMPEG4VDEC_Params);
-        ((IMPEG4VDEC_Params *)_codecParams)->outloopDeBlocking = IMPEG4VDEC_ENHANCED_DEBLOCK_ENABLE;
-        ((IMPEG4VDEC_Params *)_codecParams)->errorConcealmentEnable = IMPEG4VDEC_EC_ENABLE;
-        ((IMPEG4VDEC_Params *)_codecParams)->sorensonSparkStream = FALSE;
-        ((IMPEG4VDEC_Params *)_codecParams)->debugTraceLevel = IMPEG4VDEC_DEBUGTRACE_LEVEL0; // 0 - 2
-        ((IMPEG4VDEC_Params *)_codecParams)->lastNFramesToLog = IMPEG4VDEC_MINNUM_OF_FRAME_LOGS;
-        ((IMPEG4VDEC_Params *)_codecParams)->paddingMode = IMPEG4VDEC_MPEG4_MODE_PADDING;//IMPEG4VDEC_DIVX_MODE_PADDING;
-        ((IMPEG4VDEC_Params *)_codecParams)->enhancedDeBlockingQp = 15; // 1 - 31
-        ((IMPEG4VDEC_Params *)_codecParams)->decodeOnlyIntraFrames = IMPEG4VDEC_DECODE_ONLY_I_FRAMES_DISABLE;
-        _codecHandle = VIDDEC3_create(_codecEngine, (String)"ivahd_mpeg4dec", _codecParams);
+		break;
+	case CODEC_ID_MPEG4:
+		_frameWidth = ALIGN2(_frameWidth + 32, 7);
+		_frameHeight = _frameHeight + 32;
+		_codecParams->size = sizeof(IMPEG4VDEC_Params);
+		((IMPEG4VDEC_Params *)_codecParams)->outloopDeBlocking = IMPEG4VDEC_ENHANCED_DEBLOCK_ENABLE;
+		((IMPEG4VDEC_Params *)_codecParams)->errorConcealmentEnable = IMPEG4VDEC_EC_ENABLE;
+		((IMPEG4VDEC_Params *)_codecParams)->sorensonSparkStream = FALSE;
+		((IMPEG4VDEC_Params *)_codecParams)->debugTraceLevel = IMPEG4VDEC_DEBUGTRACE_LEVEL0; // 0 - 2
+		((IMPEG4VDEC_Params *)_codecParams)->lastNFramesToLog = IMPEG4VDEC_MINNUM_OF_FRAME_LOGS;
+		((IMPEG4VDEC_Params *)_codecParams)->paddingMode = IMPEG4VDEC_MPEG4_MODE_PADDING;//IMPEG4VDEC_DIVX_MODE_PADDING;
+		((IMPEG4VDEC_Params *)_codecParams)->enhancedDeBlockingQp = 15; // 1 - 31
+		((IMPEG4VDEC_Params *)_codecParams)->decodeOnlyIntraFrames = IMPEG4VDEC_DECODE_ONLY_I_FRAMES_DISABLE;
+		_codecHandle = VIDDEC3_create(_codecEngine, (String)"ivahd_mpeg4dec", _codecParams);
 		log->printf("DecoderVideoLibDCE::init(): using ivahd_mpeg4dec\n");
-        break;
-    case CODEC_ID_MPEG1VIDEO:
-    case CODEC_ID_MPEG2VIDEO:
-    	_codecParams->size = sizeof(IMPEG2VDEC_Params);
-        ((IMPEG2VDEC_Params *)_codecParams)->ErrorConcealmentON = IMPEG2VDEC_EC_DISABLE; // IMPEG2VDEC_EC_ENABLE
-        ((IMPEG2VDEC_Params *)_codecParams)->outloopDeBlocking =  IMPEG2VDEC_DEBLOCK_ENABLE;
-        ((IMPEG2VDEC_Params *)_codecParams)->debugTraceLevel = 0; // 0 - 4
-        ((IMPEG2VDEC_Params *)_codecParams)->lastNFramesToLog = 0;
-    	_codecHandle = VIDDEC3_create(_codecEngine, (String)"ivahd_mpeg2vdec", _codecParams);
+		break;
+	case CODEC_ID_MPEG1VIDEO:
+	case CODEC_ID_MPEG2VIDEO:
+		_codecParams->size = sizeof(IMPEG2VDEC_Params);
+		((IMPEG2VDEC_Params *)_codecParams)->ErrorConcealmentON = IMPEG2VDEC_EC_DISABLE; // IMPEG2VDEC_EC_ENABLE
+		((IMPEG2VDEC_Params *)_codecParams)->outloopDeBlocking =  IMPEG2VDEC_DEBLOCK_ENABLE;
+		((IMPEG2VDEC_Params *)_codecParams)->debugTraceLevel = 0; // 0 - 4
+		((IMPEG2VDEC_Params *)_codecParams)->lastNFramesToLog = 0;
+		_codecHandle = VIDDEC3_create(_codecEngine, (String)"ivahd_mpeg2vdec", _codecParams);
 		log->printf("DecoderVideoLibDCE::init(): using ivahd_mpeg2vdec\n");
 		break;
-    case CODEC_ID_WMV3:
-    case CODEC_ID_VC1:
-    	_frameWidth = ALIGN2(_frameWidth + (32 * 2), 7);
-    	_frameHeight = (ALIGN2(_frameHeight / 2, 4) * 2) + 2 * 40;
+	case CODEC_ID_WMV3:
+	case CODEC_ID_VC1:
+		_frameWidth = ALIGN2(_frameWidth + (32 * 2), 7);
+		_frameHeight = (ALIGN2(_frameHeight / 2, 4) * 2) + 2 * 40;
 		_codecParams->size = sizeof(IVC1VDEC_Params);
 		((IVC1VDEC_Params *)_codecParams)->errorConcealmentON = TRUE;
 		((IVC1VDEC_Params *)_codecParams)->frameLayerDataPresentFlag = FALSE;
-        ((IVC1VDEC_Params *)_codecParams)->debugTraceLevel = 0; // 0 - 4
-        ((IVC1VDEC_Params *)_codecParams)->lastNFramesToLog = 0;
+		((IVC1VDEC_Params *)_codecParams)->debugTraceLevel = 0; // 0 - 4
+		((IVC1VDEC_Params *)_codecParams)->lastNFramesToLog = 0;
 		_codecHandle = VIDDEC3_create(_codecEngine, (String)"ivahd_vc1vdec", _codecParams);
 		log->printf("DecoderVideoLibDCE::init(): using ivahd_vc1dec\n");
-        break;
-    default:
+		break;
+	default:
 		log->printf("DecoderVideoLibDCE::init(): Unsupported codec %08x\n", _codecId);
 		goto fail;
-    }
+	}
 
-    if (!_codecHandle) {
+	if (!_codecHandle) {
 		log->printf("DecoderVideoLibDCE::init(): Error: VIDDEC3_create() failed\n");
 		goto fail;
-    }
+	}
 
-    _codecStatus = (VIDDEC3_Status *)dce_alloc(sizeof(VIDDEC3_Status));
-    _codecDynParams = (VIDDEC3_DynamicParams *)dce_alloc(sizeof(VIDDEC3_DynamicParams));
-    _codecInputBufs = (XDM2_BufDesc *)dce_alloc(sizeof(XDM2_BufDesc));
-    _codecOutputBufs = (XDM2_BufDesc *)dce_alloc(sizeof(XDM2_BufDesc));
-    _codecInputArgs = (VIDDEC3_InArgs *)dce_alloc(sizeof(VIDDEC3_InArgs));
-    _codecOutputArgs = (VIDDEC3_OutArgs *)dce_alloc(sizeof(VIDDEC3_OutArgs));
-    if (!_codecDynParams || !_codecStatus || !_codecInputBufs || !_codecOutputBufs || !_codecInputArgs || !_codecOutputArgs) {
+	_codecStatus = (VIDDEC3_Status *)dce_alloc(sizeof(VIDDEC3_Status));
+	_codecDynParams = (VIDDEC3_DynamicParams *)dce_alloc(sizeof(VIDDEC3_DynamicParams));
+	_codecInputBufs = (XDM2_BufDesc *)dce_alloc(sizeof(XDM2_BufDesc));
+	_codecOutputBufs = (XDM2_BufDesc *)dce_alloc(sizeof(XDM2_BufDesc));
+	_codecInputArgs = (VIDDEC3_InArgs *)dce_alloc(sizeof(VIDDEC3_InArgs));
+	_codecOutputArgs = (VIDDEC3_OutArgs *)dce_alloc(sizeof(VIDDEC3_OutArgs));
+	if (!_codecDynParams || !_codecStatus || !_codecInputBufs || !_codecOutputBufs || !_codecInputArgs || !_codecOutputArgs) {
 		log->printf("DecoderVideoLibDCE::init(): Failed allocation with dce_alloc()\n");
 		goto fail;
-    }
-    if (_codecId == CODEC_ID_H264) {
-    	((IH264VDEC_DynamicParams *)_codecDynParams)->deblockFilterMode = IH264VDEC_DEBLOCK_DEFAULT;
-    }
+	}
+	if (_codecId == CODEC_ID_H264) {
+		((IH264VDEC_DynamicParams *)_codecDynParams)->deblockFilterMode = IH264VDEC_DEBLOCK_DEFAULT;
+	}
 
-    _codecDynParams->size = sizeof(VIDDEC3_DynamicParams);
-    _codecDynParams->decodeHeader = XDM_DECODE_AU;
-    _codecDynParams->displayWidth = 0;
-    _codecDynParams->frameSkipMode = IVIDEO_NO_SKIP;
-    _codecDynParams->newFrameFlag = XDAS_TRUE;
-    _codecDynParams->lateAcquireArg = 0;
-    if (_codecId == CODEC_ID_MPEG4 || _codecId == CODEC_ID_VC1 || _codecId == CODEC_ID_WMV3) {
-    	_codecDynParams->lateAcquireArg = -1;
-    }
+	_codecDynParams->size = sizeof(VIDDEC3_DynamicParams);
+	_codecDynParams->decodeHeader = XDM_DECODE_AU;
+	_codecDynParams->displayWidth = 0;
+	_codecDynParams->frameSkipMode = IVIDEO_NO_SKIP;
+	_codecDynParams->newFrameFlag = XDAS_TRUE;
+	_codecDynParams->lateAcquireArg = 0;
+	if (_codecId == CODEC_ID_MPEG4 || _codecId == CODEC_ID_VC1 || _codecId == CODEC_ID_WMV3) {
+		_codecDynParams->lateAcquireArg = -1;
+	}
 
-    _codecStatus->size = sizeof(VIDDEC3_Status);
-    _codecInputArgs->size = sizeof(VIDDEC3_InArgs);
-    _codecOutputArgs->size = sizeof(VIDDEC3_OutArgs);
+	_codecStatus->size = sizeof(VIDDEC3_Status);
+	_codecInputArgs->size = sizeof(VIDDEC3_InArgs);
+	_codecOutputArgs->size = sizeof(VIDDEC3_OutArgs);
 
-    codecError = VIDDEC3_control(_codecHandle, XDM_SETPARAMS, _codecDynParams, _codecStatus);
-    if (codecError != VIDDEC3_EOK) {
+	codecError = VIDDEC3_control(_codecHandle, XDM_SETPARAMS, _codecDynParams, _codecStatus);
+	if (codecError != VIDDEC3_EOK) {
 		log->printf("DecoderVideoLibDCE::init(): VIDDEC3_control(XDM_SETPARAMS) failed %d\n", codecError);
 		goto fail;
-    }
-    codecError = VIDDEC3_control(_codecHandle, XDM_GETBUFINFO, _codecDynParams, _codecStatus);
-    if (codecError != VIDDEC3_EOK) {
+	}
+	codecError = VIDDEC3_control(_codecHandle, XDM_GETBUFINFO, _codecDynParams, _codecStatus);
+	if (codecError != VIDDEC3_EOK) {
 		log->printf("DecoderVideoLibDCE::init(): VIDDEC3_control(XDM_GETBUFINFO) failed %d\n", codecError);
 		goto fail;
-    }
+	}
 
-    _inputBufBo = (omap_bo *)omap_bo_new(_omapDev, _frameWidth * _frameHeight, OMAP_BO_WC);
-    if (!_inputBufBo) {
+	_inputBufBo = (omap_bo *)omap_bo_new(_omapDev, _frameWidth * _frameHeight, OMAP_BO_WC);
+	if (!_inputBufBo) {
 		log->printf("DecoderVideoLibDCE::init(): Failed create input buffer\n");
 		goto fail;
-    }
-    _inputBufPtr = omap_bo_map(_inputBufBo);
-    _inputBufSize = omap_bo_size(_inputBufBo);
+	}
+	_inputBufPtr = omap_bo_map(_inputBufBo);
+	_inputBufSize = omap_bo_size(_inputBufBo);
 
-    _codecInputBufs->numBufs = 1;
-    _codecInputBufs->descs[0].memType = XDM_MEMTYPE_BO;
-    _codecInputBufs->descs[0].buf = (XDAS_Int8 *)omap_bo_handle(_inputBufBo);
+	_codecInputBufs->numBufs = 1;
+	_codecInputBufs->descs[0].memType = XDM_MEMTYPE_BO;
+	_codecInputBufs->descs[0].buf = (XDAS_Int8 *)omap_bo_handle(_inputBufBo);
 	_codecInputBufs->descs[0].bufSize.bytes = omap_bo_size(_inputBufBo);
 
-    _codecOutputBufs->numBufs = 2;
-    _codecOutputBufs->descs[0].memType = XDM_MEMTYPE_BO;
-    _codecOutputBufs->descs[0].bufSize.bytes = _frameWidth * _frameHeight;
-    _codecOutputBufs->descs[1].memType = XDM_MEMTYPE_BO_OFFSET;
-    _codecOutputBufs->descs[1].buf = (XDAS_Int8 *)(_frameWidth * _frameHeight);
-    _codecOutputBufs->descs[1].bufSize.bytes = _frameWidth * (_frameHeight / 2);
+	_codecOutputBufs->numBufs = 2;
+	_codecOutputBufs->descs[0].memType = XDM_MEMTYPE_BO;
+	_codecOutputBufs->descs[0].bufSize.bytes = _frameWidth * _frameHeight;
+	_codecOutputBufs->descs[1].memType = XDM_MEMTYPE_BO_OFFSET;
+	_codecOutputBufs->descs[1].buf = (XDAS_Int8 *)(_frameWidth * _frameHeight);
+	_codecOutputBufs->descs[1].bufSize.bytes = _frameWidth * (_frameHeight / 2);
 
-    _frameBuffers = (FrameBuffer **)calloc(_numFrameBuffers, sizeof(FrameBuffer *));
+	_frameBuffers = (FrameBuffer **)calloc(_numFrameBuffers, sizeof(FrameBuffer *));
 	for (int i = 0; i < _numFrameBuffers; i++) {
 		_frameBuffers[i] = (FrameBuffer *)calloc(1, sizeof(FrameBuffer));
 		if (_display->getDisplayVideoBuffer(&_frameBuffers[i]->buffer, FMT_NV12, _frameWidth, _frameHeight) != S_OK) {
 			log->printf("DecoderVideoLibDCE::getBuffer(): Failed create output buffer\n");
 			goto fail;
-	    }
-	    _frameBuffers[i]->index = i;
-	    _frameBuffers[i]->locked = false;
+		}
+		_frameBuffers[i]->index = i;
+		_frameBuffers[i]->locked = false;
 	}
 	_initialized = true;
 
